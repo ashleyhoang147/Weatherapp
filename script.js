11771a6b90f7f4bebc8a52f45ecd946f2b45e1bc
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-08-03 20:14:41 -0400

    added API

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 var nameCity = document.getElementById("Cityname").value;
+// This is our API key
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+ Cityname +"&cnt=5&appid" + APIKey;
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+         // Log the queryURL
+         console.log(queryURL);
+
+         // Log the resulting object
+         console.log(response);
+        // Transfer content to HTML
+        var celcius = Math.round(parseFloat(response.list.temp)-273.15);
+        var fahrenheit = Math.round(((parseFloat(response.list.temp)-273.15)*1.8)+32); 
+        
+        document.getElementById('description').innerHTML = response.weather.description;
+        document.getElementById('temp').innerHTML = celcius + '&deg;';
+        document.getElementById('location').innerHTML = response.city.name;
+});  
